ee44761f17167ca9a6164e999aacc57c22649724
Mir Mirani <mir.mirani@outlook.com>
2020-07-29 12:09:39 +0100

    users can login now

--- learning_logs/forms.py
+++ learning_logs/forms.py
@@ -14,3 +14,5 @@ class EntryForm(forms.ModelForm):
         fields = ['text']
         labels = {"text":''}
         widgets = {'text': forms.Textarea(attrs={'cols':80})}
+
+
--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,5 +17,10 @@ urlpatterns = [
     path('new_topic/', views.new_topic, name='new_topic'),
 
     #adding a new entry
-    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry')
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),
+
+    # page for editing an entry
+    path('edit_entry/<int:entry_id', views.edit_entry, name='edit_entry'),
+
+    
 ]
