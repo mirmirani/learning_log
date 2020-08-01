c02013f2e0773edaac1840c94871e196576d6e69
Mir Mirani <mir.mirani@outlook.com>
2020-08-01 21:08:13 +0100

    coding coding

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
@@ -10,13 +11,14 @@ def index(request):
     return render(request, 'learning_logs/index.html')
 
 # Create your views here.
-
+@login_required
 def topics(request,):
     """ Show topics"""
-    topics = Topic.objects.order_by('date_added')
+    topics = Topic.objects.filter(owner=request.user).order_by('date_added')
     context = {'topics': topics}
     return render(request, 'learning_logs/topics.html', context)
 
+@login_required
 def topic(request, topic_id):
     "show topic and it's entries"
     topic = Topic.objects.get(id=topic_id)
@@ -24,6 +26,7 @@ def topic(request, topic_id):
     context = {'topic': topic, "entries": entries}
     return render(request, 'learning_logs/topic.html', context)
 
+@login_required
 def new_topic(request):
     """ add a new topic"""
     if request.method != 'POST':
@@ -39,6 +42,7 @@ def new_topic(request):
     context = {'form': form}
     return render(request, 'learning_logs/new_topic.html', context)
 
+@login_required
 def new_entry(request, topic_id):
     """ Add a new entry"""
     topic = Topic.objects.get(id=topic_id)
@@ -58,6 +62,7 @@ def new_entry(request, topic_id):
     context = {'topic': topic, 'form': form}
     return render(request, 'learning_logs/new_entry.html', context)
 
+@login_required
 def edit_entry(request, entry_id):
     """ editing an existing entry."""
     entry = Entry.objects.get(id=entry_id)
